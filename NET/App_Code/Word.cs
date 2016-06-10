c9277123554ac494e69a352bf962cf16614d245a
kadehels <kadeiszman@gmail.com>
2016-06-10 11:45:14 -0500

    Added back the databases and add a new context also upgraded the project to the Entity Framework 6.x

---
+++ NET/App_Code/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Word
+/// </summary>
+public class UrbanWord
+{
+    public string Word { get; set; }
+    public string Definition { get; set; }
+    public int Id { get; set;  }
+    public UrbanWord()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ NET/App_Code/WordContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for WordContext
+/// </summary>
+public class WordContext : DbContext
+{
+    public WordContext()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
